3204134c43533654923725626244fb8d6e27ce98
Anouar-91 <a.berrouane@hotmail.com>
2022-10-06 14:33:01 +0200

    authentification / security / registration / add JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,7 +16,12 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *  normalizationContext={
+ *      "groups"= {"user_read"}
+ *  }
+ * 
+ * )
  * @UniqueEntity("email", message="An User with this email already exist")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -25,14 +30,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "user_read"})
      * 
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "user_read"})
      * @Assert\NotBlank(message="Email is required !")
      * @Assert\Email(message="Email format not valid !")
      */
@@ -52,7 +57,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "user_read"})
      * @Assert\NotBlank(message="Firstname is required !")
 
      */
@@ -60,7 +65,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"customers_read", "invoices_read", "user_read"})
      * @Assert\NotBlank(message="Lastname is required !")
      */
     private $lastname;
