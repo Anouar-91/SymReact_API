3a9a738d56cb71c2a7853f4de339702a05a2521a
Anouar-91 <a.berrouane@hotmail.com>
2022-10-10 11:25:02 +0200

    secure api

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,11 +11,11 @@ import '../styles/app.css';
 // start the Stimulus application
 import '../bootstrap';
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import CustomerPage from './pages/CustomerPage';
 import CustomerPageWithPagination from './pages/CustomerPageWithPagination';
 import InvoicePage from './pages/InvoicePage';
@@ -23,20 +23,31 @@ import LoginPage from './pages/LoginPage';
 import AuthAPI from './services/AuthAPI';
 
 AuthAPI.setup();
+
+const ProtectedRoute = ({ isAuthenticated, redirectPath = '/login' }) => {
+    if (!isAuthenticated) {
+        return <Navigate to={redirectPath} replace />;
+    }
+    return <Outlet />;
+};
+
+
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated());
     console.log(isAuthenticated)
+
     return (
         <HashRouter>
-            <Navbar isAuthenticated={isAuthenticated}   onLogout={setIsAuthenticated}  />
+            <Navbar isAuthenticated={isAuthenticated} onLogout={setIsAuthenticated} />
             <main className="container pt-5">
                 <Routes>
-                <Route path="/customer" element={<CustomerPage />}  />
-                <Route path="/invoice" element={<InvoicePage />} />
-                <Route path="/login" element={ <LoginPage onLogin={setIsAuthenticated}  />} />
+                    <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+                        <Route path="/customer" element={<CustomerPage />} />
+                        <Route path="/invoice" element={<InvoicePage />} />
+                    </Route>
+                    <Route path="/login" element={<LoginPage onLogin={setIsAuthenticated} />} />
+                    <Route path="/" element={<HomePage />} />
 
-                <Route path="/" element={<HomePage />} />
-                
                 </Routes>
 
             </main>
