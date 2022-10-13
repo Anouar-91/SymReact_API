c1bb36b6dd83bccd62c44b5817339119ceb8e291
Anouar-91 <a.berrouane@hotmail.com>
2022-10-13 15:08:01 +0200

    add cache for customers

--- assets/js/pages/AddCustomerPage.jsx
+++ assets/js/pages/AddCustomerPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link,  useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import Field from "../components/forms/field";
 import axios from "axios";
 import CustomersAPI from "../services/CustomersAPI";
@@ -17,7 +17,7 @@ function AddCustomerPage(props) {
 
     const fetchCustomer = async (id) => {
         try {
-            const { firstname, lastname, email, company } =await CustomersAPI.find(id);
+            const { firstname, lastname, email, company } = await CustomersAPI.find(id);
             setCustomer({ firstname, lastname, email, company });
             setLoading(false)
 
@@ -63,10 +63,10 @@ function AddCustomerPage(props) {
         e.preventDefault();
         try {
             if (editing) {
-                const response = await axios.put("http://127.0.0.1:8000/api/customers/" + id, customer)
+                const response = await CustomersAPI.update(id, customer)
                 console.log(response)
             } else {
-                const response = await axios.post("http://127.0.0.1:8000/api/customers", customer)
+                const response = await CustomersAPI.create(customer)
                 console.log(response.data)
                 setError({})
                 navigate("/customer");
@@ -76,8 +76,8 @@ function AddCustomerPage(props) {
             navigate("/customer");
 
         } catch (error) {
-                        toast.error("Une erreur est survenue")
-
+            toast.error("Une erreur est survenue")
+            console.log(error)
             const apiErrors = {}
             error.response.data.violations.forEach((violation) => {
                 apiErrors[violation.propertyPath] = violation.message;
@@ -93,60 +93,60 @@ function AddCustomerPage(props) {
             ) : (
                 <h1 className="mb-5">Création d'un client</h1>
             )}
-                     {!loading  ?(
-            <form onSubmit={handleSubmit}>
-                <Field value={customer.lastname}
-                    onChange={handleChange}
-                    name="lastname"
-                    label="Nom de famille"
-                    error={error.lastname}
-                    placeholder="Nom de famille">
-                </Field>
-                <Field value={customer.firstname}
-                    onChange={handleChange}
-                    name="firstname"
-                    error={error.firstname}
-                    label="Prénom de famille"
-                    placeholder="Prénom de famille"
+            {!loading ? (
+                <form onSubmit={handleSubmit}>
+                    <Field value={customer.lastname}
+                        onChange={handleChange}
+                        name="lastname"
+                        label="Nom de famille"
+                        error={error.lastname}
+                        placeholder="Nom de famille">
+                    </Field>
+                    <Field value={customer.firstname}
+                        onChange={handleChange}
+                        name="firstname"
+                        error={error.firstname}
+                        label="Prénom de famille"
+                        placeholder="Prénom de famille"
 
-                ></Field>
-                <Field
-                    value={customer.email}
-                    error={error.email}
-                    onChange={handleChange}
-                    name="email"
-                    label="Email"
-                    placeholder="Adresse email"
-                    type="email"
+                    ></Field>
+                    <Field
+                        value={customer.email}
+                        error={error.email}
+                        onChange={handleChange}
+                        name="email"
+                        label="Email"
+                        placeholder="Adresse email"
+                        type="email"
 
-                ></Field>
-                <Field value={customer.company ? customer.company : ""}
-                    error={error.company}
-                    onChange={handleChange}
-                    name="company"
-                    label="Entreprise"
-                    placeholder="Entreprise du client"
-                ></Field>
-                <div className="form-group mt-3">
-                    <Link to="/customer" className="btn btn-link">Retour à la liste</Link>
-                    <button type="submit" className="btn btn-success">Enregistrer</button>
+                    ></Field>
+                    <Field value={customer.company ? customer.company : ""}
+                        error={error.company}
+                        onChange={handleChange}
+                        name="company"
+                        label="Entreprise"
+                        placeholder="Entreprise du client"
+                    ></Field>
+                    <div className="form-group mt-3">
+                        <Link to="/customer" className="btn btn-link">Retour à la liste</Link>
+                        <button type="submit" className="btn btn-success">Enregistrer</button>
+                    </div>
+                </form>
+            ) : (
+                <div className="text-center">
+                    <ThreeDots
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="#0d6efd"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{ marginLeft: '50%', transform: 'translateX(-10%)' }}
+                        wrapperClassName=""
+                        visible={true}
+                    />
                 </div>
-            </form>
-            ):(
-          <div className="text-center">
-          <ThreeDots 
-          height="80" 
-          width="80" 
-          radius="9"
-          color="#0d6efd" 
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{marginLeft:'50%', transform: 'translateX(-10%)'}}
-          wrapperClassName=""
-          visible={true}
-           />
-          </div>
 
-        )}
+            )}
         </>
     )
 }
